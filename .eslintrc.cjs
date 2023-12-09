5f4199275c47832f2d74bde834c2c89a3e9fafbf
k-chop <whelmaze@gmail.com>
2023-12-09 16:12:40 +0900

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: [".eslintrc.cjs"],
+};
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import p5 from "p5";
+
+const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  p.draw = () => {
+    p.background(220);
+    p.translate(p.width / 2, p.height / 2);
+    p.ellipse(50, 50, 80, 80);
+  };
+};
+
+new p5(sketch);

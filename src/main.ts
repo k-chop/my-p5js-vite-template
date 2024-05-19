fe912622c2a8a9135eb547ce9d22a74e208ef564
k-chop <whelmaze@gmail.com>
2024-05-19 16:10:07 +0900

    スクロールバーが出ちゃってたので消した

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,10 @@ const sketch = (p: p5) => {
     p.translate(p.width / 2, p.height / 2);
     p.ellipse(50, 50, 80, 80);
   };
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
 };
 
 new p5(sketch);
